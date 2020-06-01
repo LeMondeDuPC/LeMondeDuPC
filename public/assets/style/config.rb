5fabbf058332d44d2feecdd870c0c658c76be985
NielsTRS <trsniels@gmail.com>
2020-06-01 16:41:32 +0200

    new design

--- public/assets/style/config.rb
+++ public/assets/style/config.rb
@@ -5,8 +5,6 @@ require 'compass/import-once/activate'
 http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
-images_dir = "../images"
-javascripts_dir = "../js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
