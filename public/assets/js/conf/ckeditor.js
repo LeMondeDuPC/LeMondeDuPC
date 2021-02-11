36fe1da56aec4dd0eb5b756cbb22db01f0dc91cd
NielsTRS <trsniels@gmail.com>
2021-02-11 21:14:15 +0100

    Added lazyload to ckeditor

--- public/assets/js/conf/ckeditor.js
+++ public/assets/js/conf/ckeditor.js
@@ -2,8 +2,14 @@ CKEDITOR.on('instanceReady', function (evt) {
     evt.editor.dataProcessor.htmlFilter.addRules({
         elements: {
             img: function (el) {
-                el.attributes.alt = 'Image explicative de l\'article';
-                el.attributes.class = 'image featured';
+                el.attributes.class = 'image featured lazyload';
+                if (el.attributes.alt === '') {
+                    el.attributes.alt = 'Image explicative de l\'article';
+                }
+                if (el.attributes['data-src'] == null) {
+                    el.attributes['data-src'] = el.attributes['data-cke-saved-src'];
+                }
+                el.attributes['data-cke-saved-src'] = null;
                 reset(el);
             },
             p: function (el) {
@@ -17,12 +23,12 @@ CKEDITOR.on('instanceReady', function (evt) {
             },
         }
     });
-    this.dataProcessor.writer.selfClosingEnd = ' />';
+    evt.editor.dataProcessor.writer.selfClosingEnd = ' />';
 
     // Use line breaks for block elements, tables, and lists.
     let dtd = CKEDITOR.dtd;
     for (let e in CKEDITOR.tools.extend({}, dtd.$nonBodyContent, dtd.$block, dtd.$listItem, dtd.$tableContent)) {
-        this.dataProcessor.writer.setRules(e, {
+        evt.editor.dataProcessor.writer.setRules(e, {
             indent: true,
             breakBeforeOpen: true,
             breakAfterOpen: true,
