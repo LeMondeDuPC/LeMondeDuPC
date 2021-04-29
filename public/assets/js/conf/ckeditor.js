1f8725aea045061ce93e08ef557982d63d7639ef
NielsTRS <trsniels@gmail.com>
2021-04-29 15:15:44 +0200

    Fixed ckeditor issue due to compression

--- public/assets/js/conf/ckeditor.js
+++ public/assets/js/conf/ckeditor.js
@@ -1,1 +1,45 @@
-function reset(t){t.attributes.style=null,t.attributes.width=null,t.attributes.height=null}CKEDITOR.on("instanceReady",function(t){t.editor.dataProcessor.htmlFilter.addRules({elements:{img:function(t){t.attributes.class="image featured lazyload",""===t.attributes.alt&&(t.attributes.alt="Image explicative de l'article"),null==t.attributes["data-src"]&&(t.attributes["data-src"]=t.attributes["data-cke-saved-src"]),t.attributes["data-cke-saved-src"]=null,reset(t)},p:function(t){reset(t)},div:function(t){reset(t)},td:function(t){reset(t)}}}),t.editor.dataProcessor.writer.selfClosingEnd=" />";let e=CKEDITOR.dtd;for(let a in CKEDITOR.tools.extend({},e.$nonBodyContent,e.$block,e.$listItem,e.$tableContent))t.editor.dataProcessor.writer.setRules(a,{indent:!0,breakBeforeOpen:!0,breakAfterOpen:!0,breakBeforeClose:!0,breakAfterClose:!0})});
+CKEDITOR.on('instanceReady', function (evt) {
+    evt.editor.dataProcessor.htmlFilter.addRules({
+        elements: {
+            img: function (el) {
+                el.attributes.class = 'image featured lazyload';
+                if (el.attributes.alt === '') {
+                    el.attributes.alt = 'Image explicative de l\'article';
+                }
+                if (el.attributes['data-src'] == null) {
+                    el.attributes['data-src'] = el.attributes['data-cke-saved-src'];
+                }
+                el.attributes['data-cke-saved-src'] = null;
+                reset(el);
+            },
+            p: function (el) {
+                reset(el);
+            },
+            div: function (el) {
+                reset(el);
+            },
+            td: function (el) {
+                reset(el);
+            },
+        }
+    });
+    evt.editor.dataProcessor.writer.selfClosingEnd = ' />';
+
+    // Use line breaks for block elements, tables, and lists.
+    let dtd = CKEDITOR.dtd;
+    for (let e in CKEDITOR.tools.extend({}, dtd.$nonBodyContent, dtd.$block, dtd.$listItem, dtd.$tableContent)) {
+        evt.editor.dataProcessor.writer.setRules(e, {
+            indent: true,
+            breakBeforeOpen: true,
+            breakAfterOpen: true,
+            breakBeforeClose: true,
+            breakAfterClose: true
+        });
+    }
+});
+
+function reset(el) {
+    el.attributes.style = null;
+    el.attributes.width = null;
+    el.attributes.height = null;
+}
