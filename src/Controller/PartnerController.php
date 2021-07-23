cb944e9f33ca4cfc5a5929955b1689f4f52fc6f9
NielsTRS <trsniels@gmail.com>
2021-07-24 00:16:19 +0200

    Solved Issue

--- src/Controller/PartnerController.php
+++ src/Controller/PartnerController.php
@@ -11,6 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * Class PartnersController
